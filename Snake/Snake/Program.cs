a5498b8bebc189ad9e1926b24176f8af7ecd17b2
akaTaniS <akatanisis@gmail.com>
2016-05-08 19:48:00 +0300

    Конструкторы. Инкапсуляция

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,19 +10,15 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
+           Point p1 = new Point(1, 3, '*');
+           
+            p1.Draw();
 
-            Drow( x1, y1, sym1 );
+           Point p2 = new Point(4, 5, '#');
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
+            p2.Draw(); 
 
-            Drow(x2, y2, sym2);
-
-            Console.ReadLine();                
+           Console.ReadLine();                
         }
 
         static void Drow(int x, int y , char sym)
