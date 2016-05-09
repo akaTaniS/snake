eb42e955a4723c84f1a537ca3edc937a9615d9b6
akaTaniS <akatanisis@gmail.com>
2016-05-09 21:40:40 +0300

    "Змейка" в движении

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -8,15 +8,37 @@ namespace Snake
 {
     class Snake: Figure
     {
-        public Snake(Point tail, int leight, Direction direction)
+        Direction direction;
+
+        public Snake(Point tail, int leight, Direction _direction)
         {
+            direction = _direction;
             pList = new List<Point>();
             for(int i = 0; i< leight; i++)
             {
                 Point p = new Point(tail);
-                p.Move(i, direction);
+                p.Move(i, _direction);
                 pList.Add(p);
             }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
         }
     }
 }
